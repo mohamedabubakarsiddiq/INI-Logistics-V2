fe8262e5d8c0e5d36c080750b4d6372ebdfa93fa
Mohamed Abubakar Siddiq <siddiq.h0@gmail.com>
2026-06-25 10:01:40 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -94,4 +94,9 @@ document.getElementById(
 count;
 
 }
+const hamburger = document.querySelector(".hamburger");
+const navLinks = document.querySelector(".nav-links");
 
+hamburger.addEventListener("click", () => {
+    navLinks.classList.toggle("active");
+});
