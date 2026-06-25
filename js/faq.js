6058db03114c19ad9b41aec66ffc87d340c3b424
Mohamed Abubakar Siddiq <siddiq.h0@gmail.com>
2026-06-25 09:30:04 +0530

    Update faq.js

--- js/faq.js
+++ js/faq.js
@@ -1,1 +1,22 @@
+const questions =
+document.querySelectorAll(
+".faq-question"
+);
+
+questions.forEach(question=>{
+
+question.addEventListener(
+"click",
+()=>{
+
+const answer =
+question.nextElementSibling;
+
+answer.classList.toggle(
+"active"
+);
+
+});
+
+});
 
