d6b3a347fd06a22853a9ce0822dbdf2bc6767e24
Mohamed Abubakar Siddiq <siddiq.h0@gmail.com>
2026-06-25 09:30:39 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,97 @@
+// Logout
+
+function logout(){
+
+    sessionStorage.clear();
+
+    window.location.href = "login.html";
+
+}
+
+// Login Protection
+
+function checkLogin(){
+
+    if(!sessionStorage.getItem("isLoggedIn")){
+
+        window.location.href = "login.html";
+
+    }
+
+}
+function showToast(message){
+
+let toast =
+document.getElementById("toast");
+
+toast.innerHTML = message;
+
+toast.style.display = "block";
+
+setTimeout(()=>{
+
+toast.style.display = "none";
+
+},3000);
+
+}
+function toggleTheme(){
+
+document.body.classList.toggle(
+"dark-mode"
+);
+
+localStorage.setItem(
+"theme",
+document.body.classList.contains(
+"dark-mode"
+)
+);
+
+}
+window.onload = () => {
+
+if(
+localStorage.getItem("theme")
+=== "true"
+){
+
+document.body.classList.add(
+"dark-mode"
+);
+
+}
+
+};
+function sendMessage(){
+
+showToast(
+"Message Sent Successfully!"
+);
+
+document.querySelector(
+".contact-form form"
+).reset();
+
+}
+function showNotification(){
+
+let count =
+localStorage.getItem(
+"notifyCount"
+) || 0;
+
+count++;
+
+localStorage.setItem(
+"notifyCount",
+count
+);
+
+document.getElementById(
+"notifyCount"
+).innerText =
+count;
+
+}
 
