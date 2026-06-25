76c4b913709cfc1dd01802866d627ee41f4f4f5c
Mohamed Abubakar Siddiq <siddiq.h0@gmail.com>
2026-06-25 09:30:22 +0530

    Update login.js

--- js/login.js
+++ js/login.js
@@ -1,1 +1,32 @@
+function login(){
 
+    let username =
+        document.getElementById("username").value.trim();
+
+    let password =
+        document.getElementById("password").value.trim();
+
+    let error =
+        document.getElementById("errorMessage");
+
+    if(
+        username === "admin" &&
+        password === "admin123"
+    ){
+
+        sessionStorage.setItem(
+            "isLoggedIn",
+            "true"
+        );
+
+        window.location.href =
+            "dashboard.html";
+
+    }else{
+
+        error.innerHTML =
+            "Invalid username or password";
+
+    }
+
+}
